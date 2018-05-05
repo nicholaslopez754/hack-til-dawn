e1358980675a5eda8bf736df62a69798c41a97b0
Jose Calles <jmcallesv@gmail.com>
2018-05-05 11:30:54 -0700

    create ios project

---
+++ App-iOS/App-iOS/HomeViewController.swift
@@ -0,0 +1,25 @@
+//
+//  HomeViewController.swift
+//  App-iOS
+//
+//  Created by Jose Calles on 5/5/18.
+//  Copyright © 2018 Hack Till Dawn. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
