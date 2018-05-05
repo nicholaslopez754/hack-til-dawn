ec2715992a38cd807d7498b7faa79e769f2ada71
Nicholas Lopez <nicholaslopez754@gmail.com>
2018-05-05 15:36:57 -0700

    poc testing

--- recognize.py
+++ recognize.py
@@ -1,11 +1,23 @@
 import boto3
 import config
 
+"""
+BUCKETS
+"""
 s3 = boto3.resource('s3',aws_access_key_id=config.aws_account_id,
                          aws_secret_access_key=config.aws_account_secret,
                          region_name='us-east-1')
+train_bucket = 'crowdcam-training'
+raw_bucket = 'crowdcam-raw'
+match_butcket = 'crowdcam-matches'
 
-train_bucket = s3.Bucket('crowdcam-training')
-raw_bucket = s3.Bucket('crowdcam-raw')
-match_butcket = s3.Bucket('crowdcam-matches')
+client=boto3.client('rekognition','us-west-2')
+response = client.detect_labels(Image=
+    {
+        'S3Object': {
+            'Bucket': raw_bucket,
+            'Name': 'image3.jpeg'
+        }
+    })
 
+print(response)
