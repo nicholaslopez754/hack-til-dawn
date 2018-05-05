ea5d5fd294450c85ac18c65917ade6e76ae008f2
Nicholas Lopez <nicholaslopez754@gmail.com>
2018-05-05 15:13:42 -0700

    bucket connections

--- recognize.py
+++ recognize.py
@@ -5,8 +5,7 @@ s3 = boto3.resource('s3',aws_access_key_id=config.aws_account_id,
                          aws_secret_access_key=config.aws_account_secret,
                          region_name='us-east-1')
 
-#_photos_bucket = _s3.Bucket('crowdcam-bucket')
+train_bucket = s3.Bucket('crowdcam-training')
+raw_bucket = s3.Bucket('crowdcam-raw')
+match_butcket = s3.Bucket('crowdcam-matches')
 
-# Print out bucket names
-for bucket in s3.buckets.all():
-    print(bucket.name)
